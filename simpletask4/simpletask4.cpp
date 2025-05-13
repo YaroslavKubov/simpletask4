6b247d514ef98907741890a39ebe8fd9adc68d84
YaroslavKubov <yaroslavkubov@gmail.com>
2025-05-13 03:02:12 +0300

    Добавьте файлы проекта.

---
+++ simpletask4/simpletask4.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int a, b, c;
+
+    std::cout << "enter A:";
+    std::cin >> a;
+
+    std::cout << "enter B:";
+    std::cin >> b;
+
+    std::cout << "enter C:";
+    std::cin >> c;
+
+
+    int S = 2 * (a * b + b * c + a * c);
+
+    int V = a * b * c;
+
+    std::cout << "S = " << S << std::endl;
+    std::cout << "V = " << V;
+
+
+    return 0;
+}
